f802d3610d4080fd1dde6632a4e66365b6eeec52
UnknownDuck <nm.brink21@gmail.com>
2022-08-24 09:46:38 -0400

    added python data types

---
+++ python-Data-Types.py
@@ -0,0 +1,21 @@
+
+a = "Strings" #strings
+
+b = 3 #int
+
+c = 3.2 #float
+
+d = 1j #complex
+
+e = ["apple", "bannana","cherry"] #list
+
+f = ("apple", "bannana", "cherry") #tuple
+
+g = range(0,10) #range
+
+h = { "key1":"answer", "key2":"answer2" } #dict key:value
+
+i = {"apple", "bannana", "cherry"} #set
+
+j = True #bool
+
